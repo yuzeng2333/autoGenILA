e5ecf7dde73485c00540e7badefa618a185f55d0
hotzeng <yuzeng@umich.edu>
2020-08-15 16:26:40 -0400

    add automatic generate assumption regarding instruction and reset values

--- src/func_extract/src/clean_goal.cpp
+++ src/func_extract/src/clean_goal.cpp
@@ -1,4 +1,8 @@
 #include "global_data_struct.h"
+#include "clean_goal.h"
+#include "parse_fill.h"
+
+#define toStr(a) std::to_string(a)
 
 void clean_goal() {
   std::ifstream input(g_path+"/goal.txt");
@@ -7,10 +11,11 @@ void clean_goal() {
   uint32_t instrIdx;
   std::string writeASV;
   uint32_t bound;
+  uint32_t assumIdx = 0;
   while(std::getline(input, line)) {
     if(line.front() == '#') {
       size_t pos2 = line.find("#", 1);
-      size_t pos3 = line.find("#", pos2+1);
+      size_t pos3 = line.find(  "#", pos2+1);
       size_t pos4 = line.find("#", pos3+1);
       if(pos4 != std::string::npos) {
         toCout("Error: more than 3 # found: "+line);
@@ -30,5 +35,45 @@ void clean_goal() {
       }
       output << line << std::endl;
     }
+    if(line.front() == ')') {
+      std::string instrEncodings = get_encodings(g_instrInfo[instrIdx-1].instrEncoding);
+      std::string rstEncodings = get_encodings(g_rstVal);
+      uint32_t writeDelay = get_write_delay(g_instrInfo[instrIdx-1].writeASV, writeASV);
+      output << "assume -name zy_assume"+toStr(assumIdx++)+" {__START__ == 1 |-> ("+instrEncodings+" ##1 ( "+rstEncodings+" )[*"+toStr(writeDelay+1)+"] ) }" << std::endl;
+    }
   }
+  input.close();
+  output.close();
+}
+
+
+std::string get_encodings( const std::unordered_map<std::string, std::string> &instrEncoding ) {
+  if(instrEncoding.empty()) {
+    toCout("Error: instrEncoding is empty!");
+    abort();
+  }
+  std::string ret;
+  for(auto it = instrEncoding.begin(); it != instrEncoding.end(); it++) {
+    if(it->second == "x")
+      continue;
+    ret += it->first + " == " + it->second + " && ";
+  }
+  ret.pop_back();
+  ret.pop_back();
+  ret.pop_back();
+  return ret;
+}
+
+
+uint32_t get_write_delay(const std::set<std::pair<uint32_t, std::string>> &writeASV, std::string asv) {
+  if(writeASV.empty()) {
+    toCout("Error: writeASV is empty!");
+    abort();
+  }
+  for(auto it = writeASV.begin(); it != writeASV.end(); it++) {
+    if(it->second == asv)
+      return it->first;
+  }
+  toCout("Error:"+asv+" is not in the writeASV set!");
+  abort();
 }
--- src/func_extract/src/clean_goal.h
+++ src/func_extract/src/clean_goal.h
@@ -1,8 +1,20 @@
+#ifndef CLEAN_GOAL 
+#define CLEAN_GOAL
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <set>
+#include <utility>
+#include <unordered_map>
 
 void clean_goal();
+
+std::string get_encodings( const std::unordered_map<std::string, std::string> &instrEncoding );
+
+uint32_t get_write_delay(const std::set<std::pair<uint32_t, std::string>> &writeASV, std::string asv);
+
+#endif
