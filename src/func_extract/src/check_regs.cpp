7953d0ff88ee0ac3675a4b4165f44eb985f71b39
hotzeng <yuzeng@umich.edu>
2021-03-12 11:09:03 -0500

    change last timeIdx = bound+!

--- src/func_extract/src/check_regs.cpp
+++ src/func_extract/src/check_regs.cpp
@@ -98,7 +98,7 @@ void print_llvm_ir(std::string destAndSlice,
   // input types
   std::vector<llvm::Type *> argTy;
   // push inputs
-  for(uint32_t i = 0; i <= bound; i++)  
+  for(uint32_t i = 0; i <= bound+1; i++)  
     for(auto it = moduleInputs.begin(); it != moduleInputs.end(); it++) {
       uint32_t width = get_var_slice_width_simp(*it);
       // FIXME the start and end index may be wrong
@@ -120,7 +120,7 @@ void print_llvm_ir(std::string destAndSlice,
   // set arg name for the function
   uint32_t idx = 0;
   // FIXME the start and end index may be wrong  
-  for(uint32_t i = 0; i <= bound; i++)  
+  for(uint32_t i = 0; i <= bound+1; i++)  
     for(auto it = moduleInputs.begin(); it != moduleInputs.end(); it++) {
       uint32_t width = get_var_slice_width_simp(*it);
       toCoutVerb("set func arg: "+*it+DELIM+toStr(i));
