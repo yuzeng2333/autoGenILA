f31eb8f22ff5448e478e7478ffcd8b20a34b87b7
yuzeng <yuzeng@umich.edu>
2021-06-14 21:36:06 -0400

    add option to initialize all regs in dut

--- src/func_extract/app/tb_gen.cpp
+++ src/func_extract/app/tb_gen.cpp
@@ -26,6 +26,7 @@ uint32_t cycleLen = 10;
 uint32_t protectedInstrNum = 3;
 uint32_t simulatedInstrNum = 20;
 std::ofstream output;
+bool reset_regs = true;
 
 int main(int argc, char *argv[]) {
   g_path = argv[1];
@@ -42,14 +43,19 @@ int main(int argc, char *argv[]) {
   auto topModInfo = g_moduleInfoMap[g_topModule];
   std::string clk = topModInfo->clk;
   std::string modRst = topModInfo->rst;
-  std::string rst = "reset";
+  std::string rst = modRst;
   bool positiveRst = true;
   if(modRst == "rstn" || modRst == "resetn") positiveRst = false;
+  uint32_t rstVal = positiveRst ? 1 : 0;
   for(auto input : topModInfo->moduleInputs) {
+    if(input == modRst) continue;
     uint32_t width = get_var_slice_width_simp(input, topModInfo);
     print_reg(width, input);
   }
   print_reg(1, "zy_assert_protect");
+  print_reg(1, clk);
+  print_reg(1, rst);
+
 
   for(auto out : topModInfo->moduleOutputs) {
     uint32_t width = get_var_slice_width_simp(out, topModInfo);
@@ -57,7 +63,7 @@ int main(int argc, char *argv[]) {
   }
   
 
-  to_file("  always #"+toStr(cycleLen/2)+" "+clk+" = ~"+clk+" ;");
+  to_file("  \nalways #"+toStr(cycleLen/2)+" "+clk+" = ~"+clk+" ;\n");
 
   // module instantiation
   to_file("  "+topModInfo->name+" u0 (");
@@ -68,6 +74,7 @@ int main(int argc, char *argv[]) {
     to_file("   ,."+modRst+"(~"+rst+")");
   to_file("   ,.zy_assert_protect(zy_assert_protect)");
   for(auto input : topModInfo->moduleInputs) {
+    if(input == modRst) continue;    
     to_file("  ,."+input+"("+input+")");
   }
 
@@ -80,9 +87,17 @@ int main(int argc, char *argv[]) {
   to_file("  initial begin");
   to_file("    $dumpvars();");
 
+  // initialize regs
+  if(reset_regs) {
+    for(std::string reg : topModInfo->moduleTrueRegs) {
+      assign_value(reg, 0);
+    }
+  }
+
+
   // reset values
   assign_value(clk, 0);
-  assign_value(rst, 1);
+  assign_value(rst, rstVal);
   for(auto pair : g_nopInstr) {
     if(pair.first != clk && pair.first != rst)
       assign_value(pair.first, pair.second);
@@ -90,7 +105,7 @@ int main(int argc, char *argv[]) {
   assign_value("INSTR_IN_ZY", 0);
   assign_value("zy_assert_protect", 1);
   wait_time(100);
-  assign_value(rst, 0);
+  assign_value(rst, 1-rstVal);
 
   to_file("    // begin protected instruction");
   for(uint32_t i = 0; i < protectedInstrNum; i++)
