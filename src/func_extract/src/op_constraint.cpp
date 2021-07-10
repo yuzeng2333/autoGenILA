7e2d2ed978d2717fddd782eb8f7d407e5d7997c9
yuzeng <yuzeng@umich.edu>
2021-07-10 16:42:49 -0400

    fix bug in one-op make_llvm_instr

--- src/func_extract/src/op_constraint.cpp
+++ src/func_extract/src/op_constraint.cpp
@@ -989,6 +989,9 @@ llvm::Value* case_constraint(astNode* const node, uint32_t timeIdx,
 
   std::string caseValueStr = node->srcVec[1];
   uint32_t posOfOne = get_pos_of_one(caseValueStr);
+  std::string name1 = caseVarExpr->getName().str();
+  std::string name2 = "1";
+  toCoutVerb("compare4: "+name1+", "+name2);
   llvm::Value* iteCond = b->CreateICmpEQ(extract_func(caseVarExpr, posOfOne, posOfOne, c, b, "_", condNoinline), 
                                          llvmInt(1, 1, c), 
                                          llvm::Twine( timed_name(destAndSlice+"_;_case"+toStr(posOfOne), timeIdx) ));
@@ -1036,6 +1039,9 @@ llvm::Value* add_one_case_branch_expr(astNode* const node, llvm::Value* &caseVar
     std::string caseValueStr = node->srcVec[idx];
     uint32_t posOfOne = get_pos_of_one(caseValueStr);
     // case value
+    std::string name1 = caseVarExpr->getName().str();
+    std::string name2 = "1";
+    toCoutVerb("compare3: "+name1+", "+name2);    
     llvm::Value* iteCond = b->CreateICmpEQ(
                              extract_func(caseVarExpr, posOfOne, posOfOne, c, b, "_", condNoinline), 
                              llvmInt(1, 1, c),
@@ -1515,9 +1521,15 @@ llvm::Value* make_llvm_instr(std::shared_ptr<llvm::IRBuilder<>> &b,
     return b->CreateXor(zext(op1Expr, destWidth, c, b), zext(op2Expr, destWidth, c, b), name);
   }
   else if (op == "==") {
+    std::string name1 = op1Expr->getName().str();
+    std::string name2 = op2Expr->getName().str();
+    toCoutVerb("compare1: "+name1+", "+name2);    
     return b->CreateICmpEQ(zext(op1Expr, opWidth, c, b), zext(op2Expr, opWidth, c, b), name);
   }
   else if (op == "===") {
+    std::string name1 = op1Expr->getName().str();
+    std::string name2 = op2Expr->getName().str();
+    toCoutVerb("compare2: "+name1+", "+name2);    
     return b->CreateICmpEQ(zext(op1Expr, opWidth, c, b), zext(op2Expr, opWidth, c, b), name);
   }
   else if (op == "!=") {
@@ -1579,6 +1591,9 @@ llvm::Value* make_llvm_instr(std::shared_ptr<llvm::IRBuilder<>> &b,
 llvm::Value* make_llvm_instr(builder &b, context &c, std::string op, 
                              llvm::Value* op1Expr, uint32_t op1WidthNum,
                              const llvm::Twine &name) {
+  // FIXME: the input op1WidthNum might be wrong(in the case of !),
+  // needs to figure out the reason later
+  uint32_t width = llvm::dyn_cast<llvm::IntegerType>(op1Expr->getType())->getBitWidth();
   if(op.empty()) {
     return op1Expr;
   }
@@ -1586,16 +1601,22 @@ llvm::Value* make_llvm_instr(builder &b, context &c, std::string op,
     return b->CreateNot(op1Expr, name);
   }
   else if(op == "!") {
-    return b->CreateICmpEQ(op1Expr, llvmInt(0, op1WidthNum, c), name);
+    std::string name1 = op1Expr->getName().str();
+    std::string name2 = "0";
+    toCoutVerb("compare7: "+name1+", "+name2);
+    return b->CreateICmpEQ(op1Expr, llvmInt(0, width, c), name);
   }
   else if(op == "|") {
-    return b->CreateICmpNE(op1Expr, llvmInt(0, op1WidthNum, c), name);
+    return b->CreateICmpNE(op1Expr, llvmInt(0, width, c), name);
   }
   else if(op == "&") {
-    return b->CreateICmpEQ(b->CreateNot(op1Expr), llvmInt(0, op1WidthNum, c), name);
+    std::string name1 = op1Expr->getName().str();
+    std::string name2 = "0";
+    toCoutVerb("compare8: "+name1+", "+name2);
+    return b->CreateICmpEQ(b->CreateNot(op1Expr), llvmInt(0, width, c), name);
   }
   else if(op == "-") {
-    return b->CreateSub(llvmInt(0, op1WidthNum, c), op1Expr, name);
+    return b->CreateSub(llvmInt(0, width, c), op1Expr, name);
   }
   else {
     toCout("Not supported 1-op in make_llvm_instr, op is: "+op);
--- src/func_extract/src/tmp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yuzeng/workspace/research/autoGenILA/src/func_extract/src/tmp
-# Build directory: /home/yuzeng/workspace/research/autoGenILA/src/func_extract/src/tmp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
